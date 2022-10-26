0e3cc0b15321fe9e6ca00bb33d8ae26b75687f58
CptSoap <cowbear6598@gmail.com>
2022-10-27 07:58:28 +0800

    change tsconfig

--- lib/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { Response } from "express";
-export declare function response(res: Response, status: number, message: string, data?: any): Response<any, Record<string, any>>;
-//# sourceMappingURL=index.d.ts.map
