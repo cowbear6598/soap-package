01acacc6f8f90ee6cdf1744866ace13c3a0bd485
cowbear6598 <cowbear6598@gmail.com>
2022-10-27 09:52:27 +0800

    change ignore

---
+++ dist/index.d.ts
@@ -0,0 +1,3 @@
+import { Response } from "express";
+export declare function response(res: Response, status: number, message: string, data?: any): Response<any, Record<string, any>>;
+//# sourceMappingURL=index.d.ts.map
