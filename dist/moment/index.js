e76c72bde28c5fb1bee9a791bb6041ebcb38505c
cowbear6598 <cowbear6598@gmail.com>
2022-12-23 14:10:20 +0800

    change unix timezone calculate

--- dist/moment/index.js
+++ dist/moment/index.js
@@ -3,11 +3,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 class Moment {
     constructor() {
         this.unixTime = (timezone) => {
-            const utcTime = Math.trunc(new Date().getTime() / 1000) + new Date().getTimezoneOffset() * 60;
+            const utcTime = Math.trunc(new Date().getTime() / 1000);
             return utcTime + timezone * 60 * 60;
         };
         this.unixTimeMilliSecond = (timezone) => {
-            const utcTime = new Date().getTime() + new Date().getTimezoneOffset() * 60 * 1000;
+            const utcTime = new Date().getTime();
             return utcTime + timezone * 60 * 60 * 1000;
         };
     }
--- src/moment/index.ts
+++ src/moment/index.ts
@@ -1,11 +1,11 @@
 class Moment {
     unixTime = (timezone: number) => {
-        const utcTime = Math.trunc(new Date().getTime() / 1000) + new Date().getTimezoneOffset() * 60; // timezone 0
+        const utcTime = Math.trunc(new Date().getTime() / 1000); // timezone 0
         return utcTime + timezone * 60 * 60;
     }
 
     unixTimeMilliSecond = (timezone: number) => {
-        const utcTime = new Date().getTime() + new Date().getTimezoneOffset() * 60 * 1000; // timezone 0
+        const utcTime = new Date().getTime(); // timezone 0
         return utcTime + timezone * 60 * 60 * 1000;
     }
 }
